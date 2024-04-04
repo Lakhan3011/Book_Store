8bb2738f6a7f45f0de57d8098b6a9f0a85449dbd
Lakhan lal <lakhan191136@dei.ac.in>
2024-04-04 12:01:54 +0530

    cp-5

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://lakhanlal3011:XYmYPUbhuoP66pKN@cluster0.4uorbbr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://lakhanlal3011:XYmYPUbhuoP66pKN@cluster0.4uorbbr.mongodb.net/book_db?retryWrites=true&w=majority&appName=Cluster0";
